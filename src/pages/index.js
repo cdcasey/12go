a04e8170c09978e0e60b8e20d5984069cb00e7f6
Chris Casey <cdcasey@gmail.com>
2019-04-02 21:40:15 -0500

    POC posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,55 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => {
+  console.log("DATA",data);
+  const { edges } = data.allWordpressPost;
+
+  return (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi 12go people!!!</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    {edges.map(edge => {
+      const { node } = edge;
+      return (
+        <div key={node.id}>
+        <hr />
+        <p><strong>{node.title}</strong></p>
+        <p>{node.date}</p>
+        <p dangerouslySetInnerHTML={{ __html: node.content }}/>
+        </div>
+      )
+    })}
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+)}
+
+export const query = graphql`query
+ {
+  allWordpressPost {
+    edges {
+      node {
+        title
+        excerpt
+        date
+        author
+        path
+        tags
+        slug
+        content
+      }
+    }
+  }
+}
+`;
 
 export default IndexPage
