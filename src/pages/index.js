b283b4ee0762f03bcfbe46f51e5b0e91108e33d0
Chris Casey <cdcasey@gmail.com>
2019-04-04 08:15:49 -0500

    component reorg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,57 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from 'react';
+import { Link, graphql } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
-const IndexPage = ({data}) => {
-  console.log("DATA",data);
+const IndexPage = ({ data }) => {
+  console.log('DATA', data);
   const { edges } = data.allWordpressPost;
 
   return (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi 12go people!!!</h1>
-    {edges.map(edge => {
-      const { node } = edge;
-      return (
-        <div key={node.id}>
-          <hr />
-          <p><strong  dangerouslySetInnerHTML={{ __html: node.title }}></strong></p>
-          <p>{node.date}</p>
-          <div dangerouslySetInnerHTML={{ __html: node.content }}/>
-        </div>
-      )
-    })}
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)}
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <h1>Hi 12go people!!!</h1>
+      {edges.map(edge => {
+        const { node } = edge;
+        return (
+          <div key={node.id}>
+            <hr />
+            <p>
+              <strong dangerouslySetInnerHTML={{ __html: node.title }} />
+            </p>
+            <p>{node.date}</p>
+            <div dangerouslySetInnerHTML={{ __html: node.content }} />
+          </div>
+        );
+      })}
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  );
+};
 
-export const query = graphql`query
- {
-  allWordpressPost {
-    edges {
-      node {
-        id
-        title
-        excerpt
-        date(formatString: "MMMM DD, YYYY")
-        author
-        path
-        tags
-        slug
-        content
+export const query = graphql`
+  query {
+    allWordpressPost {
+      edges {
+        node {
+          id
+          title
+          excerpt
+          date(formatString: "MMMM DD, YYYY")
+          author
+          path
+          tags
+          slug
+          content
+        }
       }
     }
   }
-}
 `;
 
-export default IndexPage
+export default IndexPage;
