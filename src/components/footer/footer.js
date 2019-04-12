1544178f6e5b85572d7e46842f17883382b7ab3b
Chris Casey <cdcasey@gmail.com>
2019-04-11 22:30:08 -0500

    subscribe links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,8 +8,79 @@ import Tags from '../tags/tags';
 export default () => {
   return (
     <footer>
-      © {new Date().getFullYear()},{` `}
-      One to Grow On
+      <ul style={{ listStyleType: 'none' }}>
+        <li>
+          <a
+            href="http://subscribeonandroid.com/www.onetogrowonpod.com/feed/podcast"
+            title="Subscribe on Android"
+          >
+            <img
+              height="34"
+              src="https://www.onetogrowonpod.com/wp-content/uploads/2018/10/android.png"
+              alt="Subscribe on Android"
+              // style={{ border: 0 }}
+            />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://itunes.apple.com/us/podcast/coming-soon/id1437683662?i=1000420674402&mt=2&app=podcast">
+            <img
+              height="34"
+              src="https://www.onetogrowonpod.com/wp-content/uploads/2018/10/apple-podcast-lrg.png"
+              alt="Listen on Apple Podcasts"
+            />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://open.spotify.com/show/2N8ibfalcLcqK5qVOD6WKI">
+            <img
+              src="https://www.onetogrowonpod.com/wp-content/uploads/2018/10/Spotify_Logo_RGB_Green.png"
+              alt="Listen on Spotify"
+              height="34"
+            />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.stitcher.com/s?fid=240925&refid=stpr">
+            <img
+              src="https://www.onetogrowonpod.com/wp-content/uploads/2018/10/Stitcher_Listen_Badge_Color_Light_BG.png"
+              height="34"
+              alt="Listen on Stitcher"
+            />
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="http://www.onetogrowonpod.com/feed/podcast"
+            title="Subscribe on Android"
+          >
+            <img
+              height="34"
+              src="https://www.onetogrowonpod.com/wp-content/uploads/2018/10/subscribe-rss.png"
+              alt="Subscribe via RSS"
+              // style={{ border: 0 }}
+            />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.patreon.com/bePatron?u=12543074">
+            <img
+              height="34"
+              src="https://www.onetogrowonpod.com/wp-content/uploads/2018/10/become_a_patron_button@2x.png"
+              alt="Become a patron"
+            />
+          </a>
+        </li>
+      </ul>
+      <div>
+        © {new Date().getFullYear()},{` `}
+        One to Grow On
+      </div>
       <SocialMenu />
       <Tags />
     </footer>
