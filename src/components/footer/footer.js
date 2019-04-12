e63596c44ac1e0f9a8ab7352d6396658c991af1e
Chris Casey <cdcasey@gmail.com>
2019-04-11 22:44:16 -0500

    Move the links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 import SocialMenu from '../menus/socialmenu';
 import Tags from '../tags/tags';
+import SubscribeLinks from '../subscribeLinks/subscribeLinks';
 
 export default () => {
   return (
@@ -14,82 +15,7 @@ export default () => {
       </div>
       <SocialMenu />
       <Tags />
-      <ul
-        style={{
-          listStyleType: 'none',
-          display: 'flex',
-          justifyContent: 'space-evenly',
-          flexWrap: 'wrap',
-        }}
-      >
-        <li>
-          <a
-            href="http://subscribeonandroid.com/www.onetogrowonpod.com/feed/podcast"
-            title="Subscribe on Android"
-          >
-            <img
-              height="34"
-              src="https://www.onetogrowonpod.com/wp-content/uploads/2018/10/android.png"
-              alt="Subscribe on Android"
-              // style={{ border: 0 }}
-            />
-          </a>
-        </li>
-
-        <li>
-          <a href="https://itunes.apple.com/us/podcast/coming-soon/id1437683662?i=1000420674402&mt=2&app=podcast">
-            <img
-              height="34"
-              src="https://www.onetogrowonpod.com/wp-content/uploads/2018/10/apple-podcast-lrg.png"
-              alt="Listen on Apple Podcasts"
-            />
-          </a>
-        </li>
-
-        <li>
-          <a href="https://open.spotify.com/show/2N8ibfalcLcqK5qVOD6WKI">
-            <img
-              src="https://www.onetogrowonpod.com/wp-content/uploads/2018/10/Spotify_Logo_RGB_Green.png"
-              alt="Listen on Spotify"
-              height="34"
-            />
-          </a>
-        </li>
-
-        <li>
-          <a href="https://www.stitcher.com/s?fid=240925&refid=stpr">
-            <img
-              src="https://www.onetogrowonpod.com/wp-content/uploads/2018/10/Stitcher_Listen_Badge_Color_Light_BG.png"
-              height="34"
-              alt="Listen on Stitcher"
-            />
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="http://www.onetogrowonpod.com/feed/podcast"
-            title="Subscribe on Android"
-          >
-            <img
-              height="34"
-              src="https://www.onetogrowonpod.com/wp-content/uploads/2018/10/subscribe-rss.png"
-              alt="Subscribe via RSS"
-              // style={{ border: 0 }}
-            />
-          </a>
-        </li>
-
-        <li>
-          <a href="https://www.patreon.com/bePatron?u=12543074">
-            <img
-              height="34"
-              src="https://www.onetogrowonpod.com/wp-content/uploads/2018/10/become_a_patron_button@2x.png"
-              alt="Become a patron"
-            />
-          </a>
-        </li>
-      </ul>
+      <SubscribeLinks />
     </footer>
   );
 };
