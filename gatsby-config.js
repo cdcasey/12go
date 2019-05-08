1f720b34572552e2c937968a8a79637c9a63280d
Chris Casey <cdcasey@gmail.com>
2019-05-07 21:09:36 -0500

    Redirect test

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,8 +32,8 @@ module.exports = {
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-        baseUrl: 'onetogrowonpod.com',
-        protocol: 'https',
+        baseUrl: '127.0.0.1:8888',
+        protocol: 'http',
         // is it hosted on wordpress.com, or self-hosted?
         hostingWPCOM: false,
         // does your site use the Advanced Custom Fields Plugin?
