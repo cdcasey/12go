cc466f387906c6b81e1694f1504e0aefffc3dbe8
Chris Casey <cdcasey@gmail.com>
2019-09-07 22:54:27 -0500

    About improvements

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,7 @@ module.exports = {
       },
     },
     'gatsby-plugin-twitter',
+    'gatsby-plugin-styled-components',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
