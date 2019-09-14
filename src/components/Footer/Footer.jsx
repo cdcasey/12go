50a84382a10112a70ab0344dddade4d23a6c3779
Chris Casey <cdcasey@gmail.com>
2019-09-14 16:55:13 -0500

    Footer replace

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import SocialMenu from '../Menus/SocialMenu';
+import Tags from '../Tags/Tags';
+import SubscribeLinks from '../subscribeLinks/subscribeLinks';
+import FooterImage from './FooterImage';
+import styles from './footer.module.scss';
+
+export default () => {
+  return (
+    <footer>
+      <div className={styles.footer1}>
+        <div className={styles.footer1__item}>
+          <Tags />
+        </div>
+
+        <div className={styles.footer1__item}>
+          <FooterImage className={styles.footer1__logo} />
+          <div style={{ textAlign: 'center' }}>
+            © {new Date().getFullYear()},{` `}
+            One to Grow On
+          </div>
+        </div>
+
+        <div className={styles.footer1__item}>
+          <SocialMenu />
+        </div>
+      </div>
+      <SubscribeLinks />
+    </footer>
+  );
+};
