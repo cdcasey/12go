85b43639f3087a4d6272f429b3fd3bec0b1593a0
Chris Casey <cdcasey@gmail.com>
2019-10-15 20:07:51 -0500

    Three photos in Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,25 +4,16 @@ import styled from 'styled-components';
 import SidebarImage from './SidebarImage';
 
 export default () => {
-  // var arr = [];
-  // while (arr.length < 8) {
-  //   var r = Math.floor(Math.random() * 100) + 1;
-  //   if (arr.indexOf(r) === -1) arr.push(r);
-  // }
-  // document.write(arr);
-
-  function randomExcluded(min, max, excluded = max + 1) {
-    max = excluded > max ? max : max - 1;
-    var n = Math.floor(Math.random() * (max - min) + min);
-    if (n >= excluded) n++;
-    return n;
-  }
-
   const min = 1;
   const max = 7;
 
-  const imageNum1 = randomExcluded(min, max);
-  const imageNum2 = randomExcluded(min, max, imageNum1);
+  const imageNumbers = [];
+  while (imageNumbers.length < 3) {
+    const imageNumber = Math.floor(Math.random() * (max - min) + min);
+    if (imageNumbers.indexOf(imageNumber) === -1) {
+      imageNumbers.push(imageNumber);
+    }
+  }
 
   return (
     <Sidebar>
@@ -35,8 +26,9 @@ export default () => {
         <li>- Christopher</li>
         <li>- Mama Casey</li>
       </PatronList>
-      <StyledSidebarImage imageNumber={imageNum1} />
-      <StyledSidebarImage imageNumber={imageNum2} />
+      <StyledSidebarImage imageNumber={imageNumbers[0]} />
+      <StyledSidebarImage imageNumber={imageNumbers[1]} />
+      <StyledSidebarImage imageNumber={imageNumbers[2]} />
     </Sidebar>
   );
 };
