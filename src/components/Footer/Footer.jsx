699eb193ed9794180a141b66f33543b14e358da1
Chris Casey <cdcasey@users.noreply.github.com>
2019-11-03 21:35:20 -0600

    Footer grid (#21)
    * Put styles in their proper place
    
    * Spacing update
    
    * Extend divider in mobile
    
    * Get things in proper order

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Link } from 'gatsby';
 
 import { colors } from '../../constants/colors';
+import spacing from '../../constants/spacing';
 import Tags from '../Tags/Tags';
 import FooterImage from './FooterImage';
 import IconContainer from '../Icons/IconContainer';
@@ -11,11 +12,11 @@ import { breakpointsDown } from '../../constants/breakpoints';
 export default props => {
   return (
     <Footer>
-      <FooterItem>
+      <FooterTagsArea>
         <Tags />
-      </FooterItem>
+      </FooterTagsArea>
 
-      <FooterItem>
+      <FooterImageArea>
         <Link to="/">
           <MyFooterImage />
         </Link>
@@ -23,11 +24,11 @@ export default props => {
           © {new Date().getFullYear()},{` `}
           One to Grow On
         </div>
-      </FooterItem>
+      </FooterImageArea>
 
-      <FooterItem>
+      <FooterIconsArea>
         <IconContainer />
-      </FooterItem>
+      </FooterIconsArea>
     </Footer>
   );
 };
@@ -35,42 +36,48 @@ export default props => {
 const Footer = styled.footer.attrs(() => ({
   className: 'footer1',
 }))`
-  display: flex;
-  padding: 10px;
-  margin-bottom: 10px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-areas: 'tags image icons';
+  padding: ${spacing(1)};
   align-items: center;
   color: ${colors.white};
   background-color: ${colors.greenMedium};
 
-  a,
-  a:link,
-  a:visited {
-    color: ${colors.white};
-    text-decoration: none;
-  }
-
-  a:hover {
-    color: ${colors.greenDark};
-    background-color: ${colors.greenLight};
-  }
-
   ${breakpointsDown.tablet} {
-    flex-direction: column;
-    row-gap: 3rem;
-    width: 100%;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-areas: 
+      "tags"
+      "icons"
+      "image"; 
+    /* flex-direction: column; */
+    /* row-gap: ${spacing(3)}; */
+    /* width: 100%; */
   }
 `;
 
-const FooterItem = styled.div.attrs(() => ({
-  className: 'footer__item',
-}))`
-  flex: 1;
-
-  ${breakpointsDown.tablet} {
-    flex: unset;
-    width: 100%;
-  }
+const FooterTagsArea = styled.div`
+  grid-area: tags;
 `;
+
+const FooterImageArea = styled.div`
+  grid-area: image;
+`;
+
+const FooterIconsArea = styled.div`
+  grid-area: icons;
+`;
+// const FooterItem = styled.div.attrs(() => ({
+//   className: 'footer__item',
+// }))`
+//   flex: 1;
+
+//   ${breakpointsDown.tablet} {
+//     flex: unset;
+//     width: 100%;
+//   }
+// `;
 
 const MyFooterImage = styled(FooterImage).attrs(() => ({
   className: 'footer__logo',
