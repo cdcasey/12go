c674e655e6254dded08666bddb43418029860b3a
Chris Casey <cdcasey@gmail.com>
2020-09-08 07:57:23 -0500

    linting

--- src/constants/isTouchEnabled.js
+++ src/constants/isTouchEnabled.js
@@ -1,3 +1,3 @@
-export default function is_touch_enabled() {
+export default function isTouchEnabled() {
   return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
 }
