a99293432e111857ad37d38bec953901f2d21702
Chris Casey <cdcasey@users.noreply.github.com>
2022-05-15 10:49:16 -0500

    Episode nav (#80)
    * Set up next & previous on post pages
    
    * Persist number of posts showing

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,11 +2,12 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import styled from 'styled-components'
 
 import PageLayout from '../components/Layout/PageLayout'
 import SEO from '../components/SEO'
+import { breakpointsUp } from '../constants/breakpoints'
 
 const propTypes = {
   data: PropTypes.shape({
@@ -19,8 +20,10 @@ const propTypes = {
   }).isRequired,
 }
 
-const PostTemplate = ({ data }) => {
-  const post = data.wpPost
+const PostTemplate = ({ pageContext }) => {
+  console.log(pageContext)
+  const { edge } = pageContext
+  const { node: post, previous, next } = edge
   const datePublished = post.date.split('T')[0]
 
   return (
@@ -31,6 +34,26 @@ const PostTemplate = ({ data }) => {
         <time dateTime={datePublished} dangerouslySetInnerHTML={{ __html: datePublished }} />
         <PostContent dangerouslySetInnerHTML={{ __html: post.content }} />
       </article>
+
+      <PageTurners>
+        {next && (
+          <div className="nextLink" style={{ display: 'flex' }}>
+            <Link to={next.uri} style={{ display: 'flex', alignItems: 'flex-start' }}>
+              <div style={{ marginRight: '.4rem' }}>&lt;- </div>
+              <div>{next.title}</div>
+            </Link>
+          </div>
+        )}
+
+        {previous && (
+          <div className="previousLink" style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Link to={previous.uri} style={{ display: 'flex', alignItems: 'flex-start' }}>
+              <div style={{ textAlign: 'right' }}>{previous.title}</div>
+              <div style={{ marginLeft: '.4rem' }}>-&gt;</div>
+            </Link>
+          </div>
+        )}
+      </PageTurners>
     </PageLayout>
   )
 }
@@ -39,16 +62,17 @@ PostTemplate.propTypes = propTypes
 
 export default PostTemplate
 
-export const pageQuery = graphql`
-  query currentPostQuery($id: String!) {
-    wpPost(id: { eq: $id }) {
-      title
-      content
-      date
-      excerpt
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query currentPostQuery($id: String!) {
+//     wpPost(id: { eq: $id }) {
+//       id
+//       title
+//       content
+//       date
+//       excerpt
+//     }
+//   }
+// `
 
 const PostContent = styled.section`
   margin-top: 2rem;
@@ -70,4 +94,11 @@ const PostContent = styled.section`
 
 const PostTitle = styled.h1`
   font-size: 4rem;
+`
+
+const PageTurners = styled.div`
+  margin-top: 4rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  font-size: 1.4rem;
 `
